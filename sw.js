625e20bebd64887c52a76ccdcd1b63f411c30635
tecwfc <wfcwfc42@gmail.com>
2026-02-03 14:02:37 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'supervila-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
